54582aea89f1c0d44a13f9d6d278a4d83c25a776
LKress <luis.kress@web.de>
2021-06-30 21:25:30 +0200

    adopted matplotlib plot and added original figures from Flye presentation (NCM 2020)

--- scripts/create_plots.py
+++ scripts/create_plots.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-plt.rcParams.update({'font.size': 20})
+
 title_fontsize = 30
 
 # names of the assemblers
@@ -34,40 +34,47 @@ grey = "#1E90FF"#"#434c5e"
 light_grey = "#008000"#"#d8dee9"
 
 
-fig, (ax1, ax2, ax3,ax4) = plt.subplots(1, 4)
-#fig.suptitle('Comparison of Assemblers for Human Genome', fontsize = 14)
+def create_plot(filename, title_fontsize, fontsize, shape):
+    plt.rcParams.update({'font.size': fontsize})
 
-ax1.bar(flye, nga50_f, color = red, label = flye)
-ax1.bar(canu, nga50_c, color = grey, label = canu)
-ax1.bar(masurca, nga50_m, color = light_grey, label = masurca)
-ax1.set_title("NGA50", fontsize=title_fontsize)
-ax1.xaxis.set_ticklabels([])
+    fig, (ax1, ax2, ax3,ax4) = plt.subplots(1, 4)
+    #fig.suptitle('Comparison of Assemblers for Human Genome', fontsize = 14)
 
-ax2.bar(flye, ref_perc_f, color = red)
-ax2.bar(canu, ref_perc_c, color = grey)
-ax2.bar(masurca, ref_perc_m, color = light_grey)
-ax2.set_ylim([99, 99.9999])
-ax2.set_title("Ref. ident. %", fontsize=title_fontsize)
-ax2.xaxis.set_ticklabels([])
+    ax1.bar(flye, nga50_f, color = red, label = flye)
+    ax1.bar(canu, nga50_c, color = grey, label = canu)
+    ax1.bar(masurca, nga50_m, color = light_grey, label = masurca)
+    ax1.set_title("NGA50", fontsize=title_fontsize)
+    ax1.xaxis.set_ticklabels([])
 
-ax3.bar(flye, ref_cov_f, color = red, label = flye)
-ax3.bar(canu, ref_cov_c, color = grey, label = canu)
-ax3.bar(masurca, ref_cov_m, color = light_grey, label = masurca)
-ax3.set_title("Ref. coverage %", fontsize=title_fontsize)
-ax3.set_ylim([94, 97.999])
-ax3.legend(loc=(0.05,0.85))
-ax3.xaxis.set_ticklabels([])
+    ax2.bar(flye, ref_perc_f, color = red)
+    ax2.bar(canu, ref_perc_c, color = grey)
+    ax2.bar(masurca, ref_perc_m, color = light_grey)
+    ax2.set_ylim([99, 99.9999])
+    ax2.set_title("Ref. ident. %", fontsize=title_fontsize)
+    ax2.xaxis.set_ticklabels([])
 
-ax4.bar(flye, misassemblies_f, color = red)
-ax4.bar(canu, misassemblies_c, color = grey)
-ax4.bar(masurca, misassemblies_m, color = light_grey)
-ax4.set_title("Misassemblies", fontsize=title_fontsize)
-ax4.xaxis.set_ticklabels([])
+    ax3.bar(flye, ref_cov_f, color = red, label = flye)
+    ax3.bar(canu, ref_cov_c, color = grey, label = canu)
+    ax3.bar(masurca, ref_cov_m, color = light_grey, label = masurca)
+    ax3.set_title("Ref. coverage %", fontsize=title_fontsize)
+    ax3.set_ylim([94, 97.999])
+    ax3.legend(loc=(0.05,0.85))
+    ax3.xaxis.set_ticklabels([])
 
+    ax4.bar(flye, misassemblies_f, color = red)
+    ax4.bar(canu, misassemblies_c, color = grey)
+    ax4.bar(masurca, misassemblies_m, color = light_grey)
+    ax4.set_title("Misassemblies", fontsize=title_fontsize)
+    ax4.xaxis.set_ticklabels([])
 
-fig.set_size_inches(15, 11)
+    fig.set_size_inches(shape[0], shape[1])
 
-fig.tight_layout()
+    fig.tight_layout()
 
-plt.savefig("presentation/images/results_HUMAN.png", transparent = True, dpi=600, bbox_inches='tight')
-plt.savefig("poster/images/results_HUMAN.png", transparent = True, dpi=600, bbox_inches='tight')
+    plt.savefig(filename, transparent = True, dpi=600, bbox_inches='tight')
+
+# create plot for presentation
+create_plot("presentation/images/results_HUMAN.png", 20, 15, [15,8])
+# create plot for poster
+create_plot("poster/images/results_HUMAN.png", 30, 20, [15,11])
+
